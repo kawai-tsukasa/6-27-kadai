92280da5b0aa659aa80638eda4548720bd8f232d
XPS\ktc <tebasaki0705@icloud.com>
2026-06-27 14:54:31 +0900

    a

---
+++ kadai/kadai.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <time.h>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+ 
+class randNum 
+{
+public:
+	int answer;
+	void Rand();
+	
+};
+
+void randNum::Rand()
+{
+	
+	for (int i = 0; i < 4; i++)
+	{
+		int ran;
+		ran = rand() % 10;
+		printf("%d\n", ran);
+	}
+	
+}
+
+class anser 
+{
+
+};
+
+
+
+int main()
+{
+	randNum Rand();
+}
